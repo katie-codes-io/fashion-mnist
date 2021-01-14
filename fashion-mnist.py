ab8a0f0c3126e6954caeca9b42dff9b21b48b505
Katie Baker <katie.m.baker87@gmail.com>
2021-01-14 21:07:50 +0000

    Added check for limiting GPU memory growth

--- fashion-mnist.py
+++ fashion-mnist.py
@@ -1,4 +1,5 @@
 import sys
+import tensorflow as tf
 
 from models.cnn import CNN
 from models.rnn import RNN
@@ -8,6 +9,18 @@ from models.rnn import RNN
 Main file for MNIST data analysis. Use command line parameter to select model to run.
 '''
 
+# Firstly, set up GPU for run (sorts out a VRAM-related issue, see
+# https://www.tensorflow.org/guide/gpu#limiting_gpu_memory_growth)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+# Now run the main code
 if __name__ == "__main__":
     # available models
     models = {
